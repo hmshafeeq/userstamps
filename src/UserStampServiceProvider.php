ee7c4d570719873104f1ea1005d515dbf34965c3
Muhammad Shafeeq <hmshafeeq@gmail.com>
2018-08-21 11:05:16 +0500

    Merged Commit

--- src/UserStampServiceProvider.php
+++ src/UserStampServiceProvider.php
@@ -2,23 +2,21 @@
 
 namespace VelitSol\UserStamps;
 
-use Illuminate\Database\Eloquent\Builder;
+
 use Illuminate\Support\ServiceProvider;
 
 class  UserStampServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        Builder::macro('withUserStamps', function ($uTypes = []) {
-            $model = $this->getModel();
-            foreach ($uTypes as $uType) {
-                $model->addExternalMethod($uType, function () use ($uType) {
-                    return $this->belongsTo(\App\Models\User::class, $uType);
-                });
-                $this->with($uType);
-            }
-            return $this;
-        });
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/userstamps.php',
+            'userstamps'
+        );
+
+        $this->publishes([
+            __DIR__ . '/config/userstamps.php' => config_path('userstamps.php'),
+        ]);
     }
 
     public function register()
--- src/UserStampTrait.php
+++ src/UserStampTrait.php
@@ -1,11 +1,62 @@
 <?php
+
 namespace VelitSol\UserStamps;
 
-use Greabock\Tentacles\EloquentTentacle;
 
 trait UserStampTrait
 {
-   use EloquentTentacle;
+
+
+    public function getUserStamps()
+    {
+        return $this->userStamps;
+    }
+
+    protected function getRelationName($userstamp)
+    {
+        return lcfirst(join(array_map('ucfirst', explode('_', $userstamp))));
+    }
+
+    public function __call($method, $parameters)
+    {
+
+        if ($method == 'hydrate') {
+            if (count($parameters) > 0 && !empty($this->userStamps)) {
+                // get users ids
+                $userIds = collect($parameters[0])->map(function ($parameter) {
+                    foreach ($this->userStamps as $userStamp) {
+                        if (!empty($parameter->{$userStamp})) return $parameter->{$userStamp};
+                    }
+                })->unique()->toArray();
+
+                $users = \DB::table(app(config('userstamps.user_model', 'App\User::class'))
+                    ->getTable())->whereIn('id', $userIds)->get();
+                // associate users with relavent fields
+                collect($parameters[0])->each(function ($parameter) use ($users) {
+                    foreach ($this->userStamps as $userStamp) {
+                        if (!empty($parameter->{$userStamp})) {
+                            $parameter->{$this->getRelationName($userStamp)} = $users->where('id', $parameter->{$userStamp})->count() > 0 ?
+                                (array)$users->where('id', $parameter->{$userStamp})->first() : [];
+                        }
+                    }
+                });
+            }
+
+        }
+
+        if (method_exists($this, '__callAfter')) {
+            return $this->__callAfter($method, $parameters);
+        }
+
+        // Keep ownder's  ancestor functional
+        if (method_exists(parent::class, '__call')) {
+            return parent::__call($method, $parameters);
+        }
+
+        throw new BadMethodCallException('Method ' . static::class . '::' . $method . '() not found');
+    }
+
+
 }
 
 
